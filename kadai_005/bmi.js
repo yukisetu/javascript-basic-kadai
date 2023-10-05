c6cc3c17cc2227d918215953583acfbe344043e9
山﨑　悠矢 <145845327+yukisetu@users.noreply.github.com>
2023-10-05 21:30:47 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,19 @@
+//変数の宣言
+let BodyWeight;
+let Height;
+let Bmi;
+
+///値の代入
+BodyWeight ='68';
+Height ='1.7';
+Bmi ='23.529411764705884';
+
+//コンソールへの出力
+console.log(BodyWeight);
+console.log(Height);
+console.log(Bmi)
+
+//BMI指数の計算
+console.log(BodyWeight / (Height * Height));
+
+
