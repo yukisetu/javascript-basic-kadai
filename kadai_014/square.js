7190b851d8dc0801aa155140158a2dc7f78fd279
山﨑　悠矢 <145845327+yukisetu@users.noreply.github.com>
2023-10-08 14:56:06 +0900

    Add files via upload

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,14 +1,8 @@
 //　アロー関数で二乗を定義
 let argument = (square) => {
-    if (console.log(square ** 2)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+        return square ** 2;
+   
 }
 
-if (argument(10)) {
-    console.log();
-}
+ console.log(argument(10));
 
